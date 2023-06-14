3209cd39112bee9fdd15fe47fcc944a6987fd3bc
dobsonco <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>
2023-06-14 11:56:55 -0400

    Made some small changes to the program

--- remove_path.py
+++ remove_path.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import sys
 
-df = pd.read_csv(sys.path[0] + '/websites.csv')
+df = pd.read_csv(sys.path[0] + '/websites.txt')
 l = ['empty' for x in df.iloc[:,4]]
 df.iloc[:,4] = l
-df.to_csv(sys.path[0] + '/websites.csv',index=False)
+df.to_csv(sys.path[0] + '/websites.txt',index=False)
