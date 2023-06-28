3ae336563be6803ccf81d09f79738a8d86241251
dobsonco <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>
2023-06-28 13:59:43 -0400

    Made some QOL changes

--- main.py
+++ main.py
@@ -69,8 +69,7 @@ def on_stop():
    '''
    global run
    run = False
-   global stopped
-   stopped = True
+
    switch()
 
 def switch():
@@ -97,7 +96,7 @@ def connected_to_internet(url='http://www.google.com/', timeout=5):
    except:
       return False
 
-def download_url(url, save_path, chunk_size=1024, type='csv'):
+def download_url(url, save_path, chunk_size=2048, type='csv'):
    '''
    Save path is just the folder you want to download it in.
 
@@ -106,9 +105,14 @@ def download_url(url, save_path, chunk_size=1024, type='csv'):
    returns name of new file and its filepath. If downloaded is a zip, it will extract it and then 
    return the path to the folder along with the name of the folder.
    '''
+   # name = os.path.basename(save_path) + '-' + str(files_in_directory) + '.' + type
+   # filepath = os.path.join(save_path,name)
+
    files_in_directory = len(next(os.walk(save_path), (None, None, []))[2])
-   name = os.path.basename(save_path) + '-' + str(files_in_directory) + '.' + type
-   filepath = os.path.join(save_path,name)
+   dir_name = os.path.basename(save_path)
+   num_ = len([i for i,j in enumerate(dir_name) if j == '_'])
+   filename = '_'.join(dir_name.split(sep='_')[0:round(num_/2)]) + '-' + str(files_in_directory) + '.' + type
+   filepath = os.path.join(save_path,filename)
 
    r = get(url, stream=True)
    with open(filepath, 'wb') as fd:
@@ -122,7 +126,6 @@ def download_url(url, save_path, chunk_size=1024, type='csv'):
             zObject.close()
             os.unlink(filepath)
          filepath = max(glob(os.path.join(save_path, '*/')), key=os.path.getmtime)
-         name = os.path.basename(filepath)
    except:
       raise ValueError
    return filepath
@@ -162,12 +165,12 @@ def autoprocess():
       for i,vals in enumerate(to_process):
          data_path = vals
          dl_folder = data_path[0:(len(data_path)-(len(os.path.basename(data_path))))]
-         filename = os.path.basename(data_path)
+         filename = os.path.basename(data_path).split(sep='.')[0]
+         data_folder = os.path.join(dl_folder,(filename.split(sep='.')[0] + '_Data'))
 
          try:
             data = read_csv(data_path,low_memory=False)
          except:
-            data_folder = os.path.join(dl_folder,filename.split(sep='.')[0])
             if os.path.isdir(dl_folder):
                del data_path,dl_folder,filename,data_folder
                continue
@@ -180,10 +183,9 @@ def autoprocess():
             del data_folder
             continue
 
-         data_folder = os.path.join(dl_folder,(filename.split(sep='.')[0] + '_Data'))
          if not os.path.isdir(data_folder):
             os.mkdir(data_folder)
-            histogram_path = os.path.join(data_folder,'Histograms')
+            histogram_path = os.path.join(data_folder,'Hist')
             os.mkdir(histogram_path)
             plots_path = os.path.join(data_folder,'Plots')
             os.mkdir(plots_path)
@@ -202,7 +204,7 @@ def autoprocess():
                   ax.set_facecolor('#ADD8E6')
                   ax.set_axisbelow(True)
                   ax.yaxis.grid(color='white', linestyle='-')
-                  savepath = os.path.join(histogram_path,(filename+'_Histogram-'+str(j + 1)+'.png'))
+                  savepath = os.path.join(histogram_path,(filename+'_Hist-'+str(j + 1)+'.png'))
                   fig.savefig(savepath,format='png')
                   plt.cla()
                   plt.clf()
@@ -425,5 +427,6 @@ window.after(100,autoprocess)
 
 window.mainloop()
 
-if stopped:
-   print('successfully exited program')
+while True:
+   if stopped:
+      exit('successfully exited program')
