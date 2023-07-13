4c483d73fe2c69df6aef8185430855e821598fcf
dobsonco <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>
2023-07-13 09:47:32 -0400

    Improved remove_path

--- Dev_Tools/remove_path.py
+++ Dev_Tools/remove_path.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import sys
+import os
+import shutil
 
 df = pd.read_csv(sys.path[0] + '/../websites.csv')
 df.iloc[:,4] = ['empty' for x in df.iloc[:,4]]
 df.to_csv(sys.path[0] + '/../websites.csv',index=False)
+
+Data_Path = os.path.join(sys.path[0],'..','Data')
+shutil.rmtree(Data_Path)
