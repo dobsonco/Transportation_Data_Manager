e3de400c3144ba8c9f6598acd56a511e4495dffc
dobsonco <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>
2023-07-17 15:51:59 -0400

    Forgot to clear paths

--- Dev_Tools/remove_path.py
+++ Dev_Tools/remove_path.py
@@ -7,8 +7,8 @@ df = pd.read_csv(sys.path[0] + '/../websites.csv')
 df.iloc[:,4] = ['empty' for x in df.iloc[:,4]]
 df.to_csv(sys.path[0] + '/../websites.csv',index=False)
 
-try:
-    Data_Path = os.path.join(sys.path[0],'..','Data')
-    shutil.rmtree(Data_Path)
-except:
-    print('Could not remove Data')
+# try:
+#     Data_Path = os.path.join(sys.path[0],'..','Data')
+#     shutil.rmtree(Data_Path)
+# except:
+#     print('Could not remove Data')
